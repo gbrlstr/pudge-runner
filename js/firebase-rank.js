424802ef0c4ddd08f6ed78f04a72eeb797e79b15
Gabriel Carlos <gabrielcarlos91@hotmail.com>
2025-08-30 17:47:40 -0300

    feat: add score validation in saveScore function and improve readability in getTopScores

--- js/firebase-rank.js
+++ js/firebase-rank.js
@@ -8,11 +8,14 @@ const db = getFirestore(app);
 const rankCollection = collection(db, "ranking");
 
 export async function saveScore(name, score) {
-  await addDoc(rankCollection, { name, score, date: new Date().toISOString() });
+  if(score > 0) {
+    await addDoc(rankCollection, { name, score, date: new Date().toISOString() });
+  }
 }
 
 export async function getTopScores(top = 10) {
   const q = query(rankCollection, orderBy("score", "desc"), limit(top));
   const snapshot = await getDocs(q);
-  return snapshot.docs.map(doc => doc.data());
+  const topScores = snapshot.docs.map(doc => doc.data());
+  return topScores;
 }
